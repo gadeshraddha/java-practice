63af58a87acc0caf533d3c4e23cbdaacb0be29ae
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-13 21:33:41 +0530

    The if statement

---
+++ 05-FlowControl/TheIfStatement.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TheIfStatement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // code here
+        if (n > 100) {
+            System.out.println("Big");
+        } 
+            System.out.println("Number");
+    }
+}
