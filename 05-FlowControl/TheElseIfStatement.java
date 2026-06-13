2a83939472735722223dd8a5a755409d00c8acea
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-13 21:39:54 +0530

    The else if Statement

---
+++ 05-FlowControl/TheElseIfStatement.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TheElseIfStatement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number > 100) {
+            System.out.println("Big");
+        } else if (number < 10) {
+            System.out.println("Small");
+        } else {
+            System.out.println("Number");
+        }
+
+        sc.close();
+    }
+}
