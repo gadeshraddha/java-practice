ae1f16a02552c89396dc2ac13e86e0e0d55216ae
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-13 22:49:20 +0530

    The greatest number among the three

---
+++ 05-FlowControl/GreatestNumber.java
@@ -0,0 +1,22 @@
+//greatest of three numbers
+
+import java.util.Scanner;
+
+class GreatestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // code here
+        if (a>=b && a>=c) {
+            System.out.println(a + " is the greatest number");
+        } else if (b>=a && b>=c) {
+            System.out.println(b + " is the greatest number");
+        } else {
+            System.out.println(c + " is the greatest number");
+        }
+    }
+}
