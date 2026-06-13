8097d46434b452228fa67027bedb6cb67ab52050
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-13 22:45:19 +0530

    The Even Odd Game

---
+++ 05-FlowControl/EvenOddGame.java
@@ -0,0 +1,21 @@
+/*Given a number n, number of apples in a bag. You and your friend are picking one apple turnwise from the bag. It is given that the first attempt is always by you. The person picking the last apple will be the winner. 
+
+If you will win: print "You" (without quotes)
+If your friend will win: print "Friend" (without quotes)*/
+
+import java.util.Scanner;
+
+class EvenOddGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        if ( n % 2 == 1) {
+            System.out.println("You");
+        } else {
+            System.out.println("Friend");
+        }
+        sc.close();
+    }
+}
