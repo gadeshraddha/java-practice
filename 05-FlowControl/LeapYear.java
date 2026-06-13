2664ee9782c02dc82c241473416d37d787bb677a
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-13 22:53:53 +0530

    Program to guess leap year

---
+++ 05-FlowControl/LeapYear.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        // code here
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("Leap year");
+                }else {
+                    System.out.println("not a leap year");
+                }
+            } else {
+                System.out.println("leap year");
+            }
+        }else {
+            System.out.println("not a leap year");
+        }
+    }
+}
