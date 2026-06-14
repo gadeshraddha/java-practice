398137c5368c2bc8c015c1eeff8f0cf3a69f5b54
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-14 19:05:42 +0530

    minor changes to all files

--- 04-Operators/LastDigitOfANumber.java
+++ 04-Operators/LastDigitOfANumber.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
-class GFG {
+class LastDigitOfANumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter n: ");
         int n = sc.nextInt();
 
-        // code here
+       
         int ans = (n % 10);
         if (ans < 0)
         ans = (-ans);
         
-        System.out.println(ans);
+        System.out.println(" the last digit of the number is " + ans);
     }
 }
