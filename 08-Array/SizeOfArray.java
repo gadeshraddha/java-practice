8653850555f550c74d75253aa1c1cc7c84146835
Shraddha Gade <gadeshraddha@yahoo.com>
2026-07-02 01:19:10 +0530

    made dir array (SizeOfArray) , string, classes&objects, oop

---
+++ 08-Array/SizeOfArray.java
@@ -0,0 +1,13 @@
+//Given an array arr[], find the size of the array.
+import java.util.Scanner;
+class SizeOfArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {7, 8, 9, 10, 56};
+
+        int size = arr.length;
+
+        System.out.println("Size of the array is: " + size);
+    }
+}
