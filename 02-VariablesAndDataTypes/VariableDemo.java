2f06666818a2ee3df2a133d51872776bf43b8b8b
Shraddha Gade <gadeshraddha@yahoo.com>
2026-09-19 22:52:51 +0530

    the big 4 primitive variable data types

---
+++ 02-VariablesAndDataTypes/VariableDemo.java
@@ -0,0 +1,29 @@
+public class VariableDemo {
+    public static void main (String[] args) {
+        
+        // int: 4 bytes, whole numbers
+        int age = 21;
+        int sector = 18;
+
+        // double: 8 bytes, decimal numbers
+        double temp = 101.4;
+        double price = 10.99;
+
+        // boolean: 1 bit, true or false
+        boolean isValid = true;
+        boolean isAdult = false;
+
+        //char: 2 bytes, single character
+        char grade = 'A';
+        char carModel = 'R';
+
+        System.out.println(" Age: " + age);
+        System.out.println(" Sector: " + sector);
+        System.out.println(" Temperature: " + temp);
+        System.out.println(" Price: " + price);
+        System.out.println(" Is Valid: " + isValid);
+        System.out.println(" Is Adult: " + isAdult);
+        System.out.println(" Grade: " + grade);
+        System.out.println(" Car Model: " + carModel);
+    }
+}
